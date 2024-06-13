d3b339d93c02c5236d7b0087d6a8ac2532aebee7
Guilherme <guilherme@photogauge.com>
2024-06-13 16:15:51 -0300

    chore: init melos and core package

---
+++ tvmaze_core/lib/tvmaze_core.dart
@@ -0,0 +1,4 @@
+export 'package:equatable/equatable.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:get_it/get_it.dart';
+export 'package:hive/hive.dart';
---
+++ tvmaze_series/lib/tvmaze_series.dart
@@ -0,0 +1,7 @@
+library tvmaze_series;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
